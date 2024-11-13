8ab21aefa5ffe5b15fe6c96b25afb1530cceedf3
leamsi360 <ismael.v.g.05@gmail.com>
2024-11-13 20:45:00 +0100

    add: version 0.1.0

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+
+  int pot1 = analogRead(A0);
+  int pot2 = analogRead(A1);
+  int pot3 = analogRead(A2);
+
+  pot1 = map(pot1, 0, 1023, 0, 255);
+  pot2 = map(pot2, 0, 1023, 0, 255);
+  pot3 = map(pot3, 0, 1023, 0, 255);
+
+  analogWrite(3, pot1);
+  analogWrite(5, pot2);
+  analogWrite(6, pot3);
+
+  Serial.print("Pot1: ");
+  Serial.print(pot1);
+  Serial.print(" Pot2: ");
+  Serial.print(pot2);
+  Serial.print(" Pot3: ");
+  Serial.println(pot3);
+}
